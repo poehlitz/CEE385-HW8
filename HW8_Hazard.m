b24b44c22494a4a92d1703e8ed1ab1aed6264e6a
poehlitz <43585265+poehlitz@users.noreply.github.com>
2018-11-25 16:46:33 -0800

    Hazard Curves

---
+++ HW8_Hazard.m
@@ -0,0 +1,9 @@
+clear all, close all
+%hazard = xlsread('HW6_Hazard.xlsx','A10:B1809')
+load('HW8_hazardcurve.mat')
+
+% Mean Annual Frequency for MCE Event
+MAF = -log(1 - 0.02) / 50;
+% Extrapolate to Intensity Measure at MCE Level
+IM_MCE = interp1(hazard(:,2),hazard(:,1),MAF);
+
